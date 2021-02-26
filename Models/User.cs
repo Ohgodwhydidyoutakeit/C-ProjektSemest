58a4ffabe885037b4ff5c5134a83217b323cd539
Witold Lipowski <AD-lipowskiw@styler.local>
2021-02-27 00:02:20 +0100

    User mode

---
+++ Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stamps.Models
+{
+    /// <summary>
+    ///  user Model 
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Each user can have multiple stamps 
+        /// </summary>
+        public ICollection<Stamp> Stamps { get; set; }
+
+        public User() { }
+        
+        /// <summary>
+        ///  Creating user -- no verification whatsoever
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="email"> </param>
+        public User(string userName, string email)
+        {
+            Username = userName;
+            Email = email;
+        }
+    }
+}
