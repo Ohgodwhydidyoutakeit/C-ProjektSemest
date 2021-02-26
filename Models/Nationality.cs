648388360e72493128c24a0df8ecce772df553a4
Witold Lipowski <AD-lipowskiw@styler.local>
2021-02-27 00:01:02 +0100

    Nationality model

---
+++ Models/Nationality.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stamps.Models
+{
+    /// <summary>
+    /// Simple Nationality model with no constraints 
+    /// </summary>
+    public class Nationality
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public Nationality() { }
+
+        public Nationality(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
